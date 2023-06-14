4ca5215259d667290fb5a488efdbae0886192dd5
Dean Cobb <51478242+nhsbsa-dean@users.noreply.github.com>
2023-06-14 13:59:55 +0100

    Header + V6

--- app/routes.js
+++ app/routes.js
@@ -10,7 +10,11 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Version Routes Files Below
 // =======================================
 
+// Current version - matching close to what is currently live
 router.use('/current', require('./views/current/_routes'));
+
+// Iterations
+router.use('/v6', require('./views/v6/_routes'));
 
 
 module.exports = router;
