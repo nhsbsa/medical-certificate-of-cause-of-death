9ca433b4d995232ee25202cd5986b04d61ee9a8b
Zoe-Slater <zoe.slater@hippodigital.co.uk>
2024-02-07 11:40:27 +0000

    Welsh/bilingual

--- app/routes.js
+++ app/routes.js
@@ -5,11 +5,6 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
-// const express = require('express');
-// const cookieParser = require('cookie-parser');
-const i18n = require('i18n');
-const nunjucks = require('nunjucks');
-// const app = module.exports = express();
 
 // =======================================
 // Version Routes Files Below
@@ -27,44 +22,6 @@ router.use('/v8', require('./views/v8/_routes'));
 router.use('/v7', require('./views/v7/_routes'));
 router.use('/v6', require('./views/v6/_routes'));
 
-
-i18n.configure({
-  // setup some locales - other locales default to en silently
-  locales: ['en', 'cy'],
-  queryParameter: "lang",
-  // sets a custom cookie name to parse locale settings from
-  cookie: 'lang_cookie_name',
-
-  // where to store json files - defaults to './locales'
-  directory: __dirname + '/locales'
-});
-
-// set default express engine and extension
-// app.engine('html', nunjucks.render);
-// app.set('view engine', 'njk');
-
-// // you will need to use cookieParser to expose cookies to req.cookies
-// app.use(cookieParser());
-
-// // i18n init parses req for language headers, cookies, etc.
-// app.use(i18n.init);
-
-
-// configure nunjucks environment
-// const env = nunjucks.configure('views', {
-//   express: app //integrate nunjucks into express
-// });
-// env.addGlobal("__", i18n.__);
-// env.addFilter("t", i18n.__);
-
-
-// // serving homepage
-// app.get('/', function (req, res) {
-//   const templateData = {
-//     date: new Date()
-//   };
-//   res.render('home', {templateData});
-// });
 
 
 module.exports = router;
