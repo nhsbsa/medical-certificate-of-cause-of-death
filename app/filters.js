fbbaec581d154607ee658cf3ac8c2d0db393a4bb
jameslattimore <jameslattimore@outlook.com>
2024-02-26 09:08:09 +0000

    Fixing the NHS numbers in the patient data.

--- app/filters.js
+++ app/filters.js
@@ -27,6 +27,85 @@ addFilter( 'getDashboardCaption', function( content ){
 });
 
 
+
+
+//
+// FILTER CONTENT BY SEARCH TERM FUNCTION
+//
+const _filterContentBySearchTerm = function( content, searchTerm ) {
+
+    const rows = [];
+    const noOfRecords = ( Array.isArray( content ) && content.length > 0 ) ? content.length : 0;
+
+    for( let i = 0; i < noOfRecords; i++ ){
+
+        let patient = content[i];
+
+        if (searchTerm) {
+
+            const regex = /^[0-9\s]+$/;
+
+            if (regex.test(searchTerm)) {
+
+                // NHS Number (only push if it matches, or is the first few digits...)
+                const spaceLessSearchTerm = searchTerm.split(' ').join('');
+                if (patient.nhsNo.split(' ').join('').indexOf(searchTerm) === 0 ) {
+                    rows.push(_getRow(patient));
+                }
+
+            } else {
+
+                // Name (allows partial matches)
+                const lowerCaseSearchTerm = searchTerm.toLowerCase();
+
+                if (patient.name.toLowerCase().indexOf(lowerCaseSearchTerm) > -1) {
+                    rows.push(_getRow(patient));
+                }
+
+            }
+
+        } else {
+
+            // No searchTerm
+            rows.push(_getRow(patient));
+
+        }
+
+
+    }
+
+    return rows;
+
+};
+
+
+//
+// GET ROW FUNCTION
+//
+const _getRow = function( patient ){
+
+    const statuses = [ 
+        '<span class="govuk-tag govuk-tag--blue">For officer review</span>',
+        '<span class="govuk-tag govuk-tag--orange">To be amended</span>',
+        '<span class="govuk-tag govuk-tag--green">For sign-off by Medical Examiner</span>',
+        '<span class="govuk-tag govuk-tag--purple">Ready to share</span>',
+        '<span class="govuk-tag">Shared</span>'
+    ];
+
+    let arr = [];
+
+    arr.push( { html: patient.name + '<br /><span class="govuk-body-s govuk"><span class="govuk-visually-hidden">NHS number: </span>' + patient.nhsNo + '</span>' } );
+    arr.push( { html: patient.placeOfDeath } );
+    arr.push( { text: patient.dateOfDeath } );
+    arr.push( { html: '<a href="#">View</a>' } ); // Temp
+    arr.push( { html: statuses[Math.round(Math.random()*(statuses.length-1))] } ); // Temp
+
+    return arr;
+
+};
+
+
+
 //
 // GET DASHBOARD TABLE ROWS
 //
@@ -36,78 +115,6 @@ addFilter('getDashboardTableRows', function( content ) {
 
     const noOfRows = ( Number.isInteger( parseInt(this.ctx.data.rowsPerPage) ) ) ? parseInt(this.ctx.data.rowsPerPage) : 10;
     const searchTerm = ( this.ctx.data.searchTerm && this.ctx.data.searchTerm.trim().length > 2 ) ? this.ctx.data.searchTerm.trim() : '';
-
-    // Just randomnly grabs one for now
-    const statuses = [ 
-        '<span class="govuk-tag govuk-tag--blue">For officer review</span>',
-        '<span class="govuk-tag govuk-tag--orange">To be amended</span>',
-        '<span class="govuk-tag govuk-tag--green">For sign-off by Medical Examiner</span>',
-        '<span class="govuk-tag govuk-tag--purple">Ready to share</span>',
-        '<span class="govuk-tag">Shared</span>'
-    ];
-
-    // Repeat this 
-    const _getRow = function( patient ){
-
-        let arr = [];
-
-        arr.push( { html: patient.name + '<br /><span class="govuk-body-s govuk"><span class="govuk-visually-hidden">NHS number: </span>' + patient.nhsNo + '</span>' } );
-        arr.push( { html: patient.placeOfDeath } );
-        arr.push( { text: patient.dateOfDeath } );
-        arr.push( { html: '<a href="#">View</a>' } ); // Temp
-        arr.push( { html: statuses[Math.round(Math.random()*(statuses.length-1))] } ); // Temp
-
-        return arr;
-
-    };
-
-    // Filter the content
-    const _filterContentBySearchTerm = function( content, searchTerm ) {
-
-        const rows = [];
-        const noOfRecords = ( Array.isArray( content ) && content.length > 0 ) ? content.length : 0;
-
-        for( let i = 0; i < noOfRecords; i++ ){
-
-            let patient = content[i];
-
-            if (searchTerm) {
-
-                const regex = /^[0-9\s]+$/;
-
-                if (regex.test(searchTerm)) {
-
-                    // NHS Number (only push if it matches)
-                    const spaceLessSearchTerm = searchTerm.split(' ').join('');
-                    if (patient.nhsNo.split(' ').join('') === spaceLessSearchTerm) {
-                        rows.push(_getRow(patient));
-                    }
-
-                } else {
-
-                    // Name (allows partial matches)
-                    const lowerCaseSearchTerm = searchTerm.toLowerCase();
-
-                    if (patient.name.toLowerCase().indexOf(lowerCaseSearchTerm) > -1) {
-                        rows.push(_getRow(patient));
-                    }
-
-                }
-
-            } else {
-
-                // No searchTerm
-                rows.push(_getRow(patient));
-
-            }
-
-
-        }
-
-        return rows;
-
-    };
-
 
     // Perform the filtering
     let rows = _filterContentBySearchTerm( content, searchTerm );
