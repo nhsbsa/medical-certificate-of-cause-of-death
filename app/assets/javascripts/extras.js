7ed271278b585c3d3b0f67cd79e8f3e5388e66be
James Lattimore <james.lattimore@nhsbsa.nhs.uk>
2024-03-17 13:48:13 +0000

    First pass through of the translation tag work.

---
+++ app/assets/javascripts/extras.js
@@ -0,0 +1,24 @@
+
+//
+// RELOADS PAGE AUTOMATICALLY WHEN USING THE BACK BUTTON
+//
+window.addEventListener( 'pageshow', function ( event ) {
+    const historyTraversal = event.persisted || ( typeof window.performance != 'undefined' && window.performance.getEntriesByType('navigation')[0].type === 'back_forward' );
+    if ( historyTraversal ) {
+      window.location.reload();
+    }
+});
+
+//
+// CHECK FOR LANGUAGE CHANGE
+//
+window.addEventListener('DOMContentLoaded',function(){
+
+  let url = new URL( window.location.href )
+  let params = new URLSearchParams(url.search);
+
+  if( params.get('lang') ){
+    history.back();
+  } 
+
+});
--- app/filters.js
+++ app/filters.js
@@ -10,9 +10,136 @@ addFilter('debugData', function(content) {
 
     // content: the session data object
 
+    delete content.translations;
+
     return (this.ctx.data.debug === 'true') ? '<div class="govuk-grid-row"><div class="govuk-grid-column-two-thirds"><h2 class="govuk-heading-m govuk-!-margin-top-9">Session data</h2><textarea class="govuk-textarea">' + JSON.stringify(content) + '</textarea><p><a class="govuk-link" href="?debug=false">Remove</a></p></div></div>' : '';
 
 });
+
+
+//
+// GET POSTCODE SEARCH SELECT
+//
+addFilter('getPostCodeSearchSelect', function( content, type ) {
+
+    // content: the array of returned values
+    // type: 'hospital' or 'another'
+
+    let currentValue = this.ctx.data['select-'+type+'-address'];
+    let html = '<select class="govuk-select" id="select-'+type+'-address" name="select-'+type+'-address">';
+
+    html += ( currentValue ) ? '<option value="">Select address</option>' : '<option selected value="">Select address</option>';
+    content.forEach(function( el ){
+        html += ( currentValue === el ) ? '<option selected>'+el+'</option>' : '<option>'+el+'</option>';
+    });
+
+    
+    return html;
+
+});
+
+
+//
+// GET POSTCODE SEARCH STATUS
+//
+addFilter('getPostCodeSearchStatus', function(content, type) {
+
+    // content: the array of returned values
+    // type: 'hospital' or 'another'
+
+    let status = ( type === 'hospital' ) ? this.ctx.data.translations.dpdHospitalPostcodeSelectResultDescription[this.ctx.data.lang] : this.ctx.data.translations.dpdOtherLocationPostcodeSelectResultDescription[this.ctx.data.lang];
+   
+    const noOfResults = ( Array.isArray( content ) ) ? content.length : 0;
+    const postCode = ( this.ctx.data[type+'-postcode'] ) ? this.ctx.data[type+'-postcode'] : 'LS1 3EX';
+
+    status = status.replace('[# results]', noOfResults);
+    status = status.replace('[postcode]', postCode);
+
+    if( noOfResults !== 1 ){
+        status = status.replace('address', 'addresses');
+        status = status.replace('cyfeiriad', 'cyfeiriadau');
+    }
+    
+    // Add in the HTML link
+    let searchAgain = ( type === 'hospital' ) ? this.ctx.data.translations.dpdHospitalPostcodeSearchAgainLink[this.ctx.data.lang] : this.ctx.data.translations.dpdOtherLocationPostcodeSearchAgainLink[this.ctx.data.lang];
+    let url = ( type === 'hospital' ) ? 'hospital-postcode' : 'another-location-postcode';
+    status = status.replace( searchAgain, '<a href="'+url+'">'+searchAgain+'</a>');
+
+    return status;
+
+});
+
+
+//
+// GET FOOTER STATUS
+//
+addFilter('getFooterStatus', function(content) {
+
+    // content: blank string
+
+    let roleType;
+    switch( this.ctx.data['role-type'] ){
+        case 'me':
+            roleType = 'as a medical examiner (ME)';
+            break;
+        case 'meo':
+            roleType = 'as a medical examiner officer (MEO)';
+            break;
+        case 'ap':
+            roleType = 'as an attending practitioner (AP)';
+            break;
+        default: 
+            roleType = 'as an unknown person';
+            break;
+    }
+
+    let path;
+    switch( this.ctx.data['over-under-28'] ) {
+        case 'dpd66Or65RadioYes':
+            path = 'on an infant MCCD path';
+            break;
+        case 'dpd66Or65RadioNo':
+            path = 'on an adult MCCD path';
+            break;
+        default:
+            path = 'on an undetermined MCCD path'
+            break;
+    }
+
+    let lang = ( this.ctx.data.lang === 'en' ) ? '(in English)' : '(in Welsh)';
+
+    return 'Currently acting ' + roleType + ' ' + path + ' ' + lang;
+
+});
+
+
+
+//
+// GET TRANSLATION TABLE ROWS
+//
+addFilter('getTranslationTableRows', function(content) {
+
+    // content: the translations variable
+
+    const rows = [];
+
+    if( content.constructor === Object ){
+
+        Object.keys(content).forEach(function(key){
+            let item = content[key];
+            rows.push( [ { html:'<input style="width:300px;font-size: 0.6em;" class="govuk-input" type="text" value="data.translations.'+key+'[data.lang]" />' }, {text:item.en}, {text:item.cy} ] );
+        });
+
+    }
+
+    return rows;
+
+});
+
+
+
+
+
 
 //
 // GET STATUS FILTER OPTIONS
