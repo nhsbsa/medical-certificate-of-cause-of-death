6a954d1f7b6303682815a321a514e1e2d2ca16d0
erikaosti <erika.osti@nhsbsa.nhs.uk>
2024-03-19 11:53:53 +0000

    MEO UT Sp6 - MEO & ME journeys

--- app/views/tests/_routes.js
+++ app/views/tests/_routes.js
@@ -2,3 +2,24 @@ const govukPrototypeKit = require('govuk-prototype-kit');
 const router = govukPrototypeKit.requests.setupRouter();
 
 module.exports = router;
+
+
+// Officer review
+router.post(/officer-review/, (req, res) => {
+    res.redirect('confirmation?role-type=meo')
+});
+
+// ME signoff > Qualifications
+router.post(/me-signoff/, (req, res) => {
+    res.redirect('qualifications')
+});
+
+    // > Qualifications > Declaration
+    router.post(/qualifications/, (req, res) => {
+        res.redirect('declaration')
+    });
+
+        // > Declaration > Confirmation page
+        router.post(/me-signoff-dec/, (req, res) => {
+            res.redirect('confirmation?role-type=me')
+        });
