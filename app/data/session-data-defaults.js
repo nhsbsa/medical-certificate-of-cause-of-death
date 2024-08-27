e2aef2911e627169c79cddbd61f8ff664830c17b
James Lattimore <james.lattimore@nhsbsa.nhs.uk>
2024-08-27 15:40:01 +0100

    Adding both versions of the intro text to V11bi (toggleable with V11[showBilingual] )

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -112,6 +112,7 @@ module.exports = {
 
     showContactMethodScreen: 'false',
     showExplanationOfStatuses: 'false',
+    showBilingual: 'true',
     
     validateTaskList: 'true',
     validateCauseOfDeathScreen: 'true',
